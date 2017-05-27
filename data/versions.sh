0ba1e175e6ffa87dd9cdb7f62687181883eb5074
Víctor Díaz Marco <victor@victordiaz.me>
2017-05-28 00:57:50 +0200

    Updated OpenSSL versions.

--- data/versions.sh
+++ data/versions.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 ## Software versions (you can edit this if there are newer versions)
-## Updated: 2017-04-25
+## Updated: 2017-05-28
 
 # Nginx
 #NGINX_VERSION="1.13.0" # 2017-04-25 (mainline)
 NGINX_VERSION="1.12.0" # 2017-04-12 (stable)
 
 # OpenSSL
-#OPENSSL_VERSION="1.1.0e" # 2017-02-16 (stable)
-OPENSSL_VERSION="1.0.2k" # 2017-01-26 (long term support)
+#OPENSSL_VERSION="1.1.0f" # 2017-05-25 (stable)
+OPENSSL_VERSION="1.0.2l" # 2017-05-25 (long term support)
 
 # PCRE
 PCRE_VERSION="8.40" # 2017-01-11 (old stable, latest supported by Nginx)
