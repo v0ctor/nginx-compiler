fe33b1cfd7c36269bf487f989ddb6105b3a479ae
Djerfy <djerfy@gmail.com>
2017-07-12 22:36:57 +0200

    Add variable pagespeed install

--- data/extras.sh
+++ data/extras.sh
@@ -5,3 +5,7 @@
 # NAXSI (anti XSS and SQL injection)
 # https://github.com/nbs-system/naxsi
 INSTALL_NAXSI="no"
+
+# PAGESPEED (automatic pagespeed optimization module)
+# https://github.com/pagespeed/ngx_pagespeed
+INSTALL_PAGESPEED="no"
