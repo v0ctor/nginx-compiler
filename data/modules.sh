bc761f218ea853c2064e78d02278a39c18ad91ee
Víctor Díaz Marco <victor@victordiaz.me>
2017-07-25 13:27:38 +0200

    Reordered modules.

--- data/modules.sh
+++ data/modules.sh
@@ -2,10 +2,10 @@
 
 ## Extra modules (you can edit this file to enable them)
 
+# PAGESPEED (automatic optimization module)
+# https://github.com/pagespeed/ngx_pagespeed
+INSTALL_PAGESPEED="no"
+
 # NAXSI (anti XSS and SQL injection)
 # https://github.com/nbs-system/naxsi
 INSTALL_NAXSI="no"
-
-# PAGESPEED (automatic pagespeed optimization module)
-# https://github.com/pagespeed/ngx_pagespeed
-INSTALL_PAGESPEED="no"
