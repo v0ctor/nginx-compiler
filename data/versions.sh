5e84c28c17c44c64bdac8613739b37e950fd87d3
Víctor Díaz Marco <victor@victordiaz.me>
2018-02-11 12:08:38 +0100

    Updated software versions.

--- data/versions.sh
+++ data/versions.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 ## Software versions (you can edit this if there are newer versions)
-## Updated: 2017-11-12
+## Updated: 2018-02-11
 
 # Nginx
-#NGINX_VERSION="1.13.6" # 2017-10-10 (mainline)
+#NGINX_VERSION="1.13.8" # 2017-12-26 (mainline)
 NGINX_VERSION="1.12.2" # 2017-10-17 (stable)
 
 # OpenSSL
 #OPENSSL_VERSION="tls1.3" # latest development version with TLS 1.3 support
 #OPENSSL_VERSION="1.1.0g" # 2017-11-02 (stable)
-OPENSSL_VERSION="1.0.2m" # 2017-11-02 (long term support)
+OPENSSL_VERSION="1.0.2n" # 2017-12-07 (long term support)
 
 # [About OpenSSL with TLSv1.3 support]
 #
@@ -27,4 +27,4 @@ PCRE_VERSION="8.41" # 2017-07-05 (old stable, latest supported by Nginx)
 ZLIB_VERSION="1.2.11" # 2017-01-15 (stable)
 
 # PageSpeed (module, optional)
-PAGESPEED_VERSION="1.12.34.3" # 2017-09-28 (stable)
+PAGESPEED_VERSION="1.13.35.2" # 2018-02-05 (stable)
